9e9593eb29b7a761a4ab9a519ab90ebcd84ce19c
MayDan12 <oluwamayowadan@gmail.com>
2024-02-21 12:43:49 +0000

    0-gather_data_from_an_api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,58 +1,49 @@
 #!/usr/bin/python3
-"""
- This python script a rest api for a given emplyee ID, and returns his informations
-"""
+'''
+This script gathers employee data from API
+'''
+
+import re
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-  """
-  This retrieve and display employee todo list progress.
-  :param employee_id: Representing Integer
-  """
-  api_endpoint = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+REST_API = "https://jsonplaceholder.typicode.com"
 
-  try:
-    # This fetch todo list for the given employee ID
-    res = requests.get(api_endpoint)
-    res.raise_for_status()
+if __name__ == '__main__':
+    # This checks if an employee is provided
+    if len(sys.argv) > 1:
+        # This validates the provided argument as a +pos integer
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            # This convert the employee ID to an integer
+            id = int(sys.argv[1])
 
-    todo_lists = res.json()
+            # This Fecth the user details and name
+            reqs = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            
+            #This fetch all the task in the todo list
+            task_reqs = requests.get('{}/todos'.format(REST_API)).json()
+            
+            # This extract the employees name
+            emp_name = reqs.get('name')
+            
+            # This filters tasks for the specified employee
+            tasks = [task for task in task_reqs if task.get('userId') == id]
+            #tasks = list(filter(lambda x: x.get('userId') == id, task_reqs))
+            
+            # This shows and filter out the employee completed tasks
+            completed_tasks = [task for task in tasks if task.get('completed')]
+            #completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            
+            # This display the employee todo list
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
 
-    # this checks if there is todolist
-    if not todo_lists:
-      print(f"No TODO lists found for employee {employee_id}")
-      return
-
-    # This extract employee name
-    employee_name = todo_lists[0].get('userId')
-
-    #completed count and total tasks
-    complete_task = sum(task.get('completed', False) for task in todo_lists)
-    task_total = len(todo_lists)
-
-    # This displays employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({complete_task}/{task_total}):")
-
-    #display titles of completed tasks
-    for task in todo_lists:
-      if task.get('completed', False):
-        print(f"\t{task['title']}")
-
-  except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
-    sys.exit(1)
-
-if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: python script.py <employee_id>")
-    sys.exit(1)
-
-  try:
-    employee_id = int(sys.argv[1])
-  
-  except ValueError:
-    print("Error: Employee ID must be an integer.")
-    sys.exit(1)
-  
-  get_employee_todo_progress(employee_id)
+            # This displays titles of the completed tasks
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
