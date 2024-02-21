65718f0df3965fc6254d58c8baaad6fa81dc463a
MayDan12 <oluwamayowadan@gmail.com>
2024-02-21 18:53:16 +0000

    1-export_to_CSV

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 This script gathers employee data from API
-'''
+"""
 
 import re
 import requests
@@ -10,31 +10,29 @@ import sys
 REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-    # This checks if an employee is provided
+    """ This checks if an employee is provided"""
     if len(sys.argv) > 1:
-        # This validates the provided argument as a +pos integer
+        """ This validates the provided argument as a +pos integer"""
         if re.fullmatch(r'\d+', sys.argv[1]):
-            # This convert the employee ID to an integer
+            """This convert the employee ID to an integer"""
             id = int(sys.argv[1])
 
-            # This Fecth the user details and name
+            """This Fecth the user details and name"""
             reqs = requests.get('{}/users/{}'.format(REST_API, id)).json()
             
-            #This fetch all the task in the todo list
+            """ This fetch all the task in the todo list"""
             task_reqs = requests.get('{}/todos'.format(REST_API)).json()
             
-            # This extract the employees name
+            """ This extract the employees name"""
             emp_name = reqs.get('name')
             
-            # This filters tasks for the specified employee
+            """ This filters tasks for the specified employee"""
             tasks = [task for task in task_reqs if task.get('userId') == id]
-            #tasks = list(filter(lambda x: x.get('userId') == id, task_reqs))
-            
-            # This shows and filter out the employee completed tasks
+                        
+            """  This shows and filter out the employee completed tasks"""
             completed_tasks = [task for task in tasks if task.get('completed')]
-            #completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            
-            # This display the employee todo list
+                        
+            """ This display the employee todo list"""
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
                     emp_name,
@@ -43,7 +41,7 @@ if __name__ == '__main__':
                 )
             )
 
-            # This displays titles of the completed tasks
+            """ This displays titles of the completed tasks"""
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
