d57ecf8ec8ca56222d45d20b30d1e58d79e0aaa9
MayDan12 <oluwamayowadan@gmail.com>
2024-03-05 23:20:05 +0000

    0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+'''
+  the function number_of_subscribers in reddit
+'''
 import requests
+from sys import argv
+
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API to retrieve the number of subscribers for a given subreddit.
-    """
-
-    urls = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "YourCustomUserAgent/1.0"}  # Set a custom User-Agent
-
+    '''
+        This methods returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(urls, header=header, allow_redirects=False)
-        response.raise_for_status()  # Raise exception for non-200 status codes
-
-        data = response.json()
-        return data.get("data", {}).get("subscribers")
-
-    except requests.exceptions.RequestException:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
 
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
